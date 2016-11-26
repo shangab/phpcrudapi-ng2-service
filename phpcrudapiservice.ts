0de7470e236cfafc2bab85b7887a85c8b71ab613
Abubaker Shangab <shangab@gmail.com>
2016-11-25 22:21:04 -0700

    Second Commit

---
+++ phpcrudapiservice.ts
@@ -0,0 +1,1 @@
+export {PhpCrudApiService} from './src/phpcrudapiservice';
---
+++ src/phpcrudapiservice.ts
@@ -0,0 +1,8 @@
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+
+
+@Injectable()
+export class PhpCrudApiService {
+
+}
